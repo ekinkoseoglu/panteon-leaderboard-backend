4906f34d91eacf0785af623ff9c4231b2951a6ad
ekinkoseoglu <ekinkoseoglu0@gmail.com>
2024-10-19 19:44:16 +0300

    Add players to leaderboard module

--- src/Business/Modules/leaderboard.module.ts
+++ src/Business/Modules/leaderboard.module.ts
@@ -3,10 +3,11 @@ import { PrismaService } from 'src/DataAccess/prisma.service';
 import { RedisModule } from '@nestjs-modules/ioredis';
 import { LeaderboardService } from '../Concrete/leaderboard.service';
 import { LeaderboardController } from 'src/Http/Controllers/leaderboard.controller';
+import { Player } from 'src/Entity/Models/player.entity';
 
 @Module({
   imports: [RedisModule],
   controllers: [LeaderboardController],
-  providers: [LeaderboardService, PrismaService],
+  providers: [LeaderboardService, PrismaService, Player],
 })
 export class LeaderboardModule {}
